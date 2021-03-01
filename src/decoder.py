5df4d86c47dc34782b1cf69ef12c8cf18ccf9024
amiryeshooroon <amiryeshooroon@gmail.com>
2021-03-01 16:06:21 +0200

    Add support for instruction prefixes

--- src/decoder.py
+++ src/decoder.py
@@ -3,11 +3,51 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     from processor import Processor
-from x86_exceptions import Unimplemented
+from x86_exceptions import Unimplemented, UnorderedPrefixList
 from instructions.instruction import *
+from bitarray import bitarray
+from enum import IntEnum
+from segmentation.selectors import Segments
+
+def check_for_prefix(instruction: bytearray):
+    prefix = int(instruction[0])
+
+    if FirstGroupPrefix.has_value(prefix):
+        return PrefixTypes.FIRST, prefix
+    elif SecondGroupPrefix.has_value(prefix):
+        return PrefixTypes.FIRST, prefix
+    elif ThirdGroupPrefix.has_value(prefix):
+        return PrefixTypes.SECOND, prefix
+    elif FourthGroupPrefix.has_value(prefix):
+        return PrefixTypes.FOURTH, prefix
+    
+    return PrefixTypes.NONE, prefix
 
 def decode_16(proc: Processor):
-    pass
+    instruction_address = proc.get_current_instruction_address()
+    offset_from_beginning = 0
+    instruction_size = 1
+    instruction = proc.read_memory(instruction_address, 1, Segments.CS)
+    prefix_list = []
+    # First check if there is an instruction prefix
+    while True:
+        prefix_type, prefix = check_for_prefix(instruction)
+        if prefix_type != PrefixTypes.NONE:
+            # Check if the prefixes are ordered
+            if len(prefix_list) > 0 and prefix_list[-1][0] > prefix_type:
+                raise UnorderedPrefixList(prefix_list[-1][0], prefix_type)
+            # Add to list of prefixes
+            prefix_list.append([prefix_type, prefix])
+            # Next byte
+            offset_from_beginning += 1
+            # Increment size
+            instruction_size += 1
+            # Read next byte
+            instruction = proc.read_memory(instruction_address + offset_from_beginning,
+            1, Segments.CS)
+        else:
+            break
+    # Now 'instruction' contains the first byte of the opcode
 
 def decode_32(proc: Processor):
     raise Unimplemented
--- src/instructions/instruction.py
+++ src/instructions/instruction.py
@@ -10,12 +10,14 @@ from x86_exceptions import InvalidOpcode, InvalidInstructionCode
 
 class InstructionCodes(IntEnum):
     MOV_RM8_R8 = 0
+    ADD_RM8_R8 = 1
 
     @classmethod
     def has_value(cls, value: InstructionCodes):
         return value in set(item.value for item in InstructionCodes)
 
 class Opcodes(IntEnum):
+    ADD_RM8_R8 = 0x0
     MOV_RM8_R8 = 0x88
     
     @classmethod
@@ -23,6 +25,7 @@ class Opcodes(IntEnum):
         return value in set(item.value for item in Opcodes)
         
 opcode_to_instruction_code: Dict[Opcodes, InstructionCodes] = {}
+instruction_code_to_instruction_object: Dict[InstructionCodes, Instruction] = {}
 
 def register_opcode(opcode: Opcodes, instruction_code: InstructionCodes):
     global opcode_to_instruction_code
@@ -32,6 +35,61 @@ def register_opcode(opcode: Opcodes, instruction_code: InstructionCodes):
         raise InvalidOpcode(opcode)
     # If so, save it
     opcode_to_instruction_code[opcode] = instruction_code
+
+def register_instruction(instruction_code: InstructionCodes, instruction_object: Type[Instruction]):
+    global instruction_code_to_instruction_object
+    
+    # First make sure that the opcode exists
+    if not InstructionCodes.has_value(instruction_code):
+        raise InvalidInstructionCode(instruction_code)
+    # If so, save it
+    instruction_code_to_instruction_object[instruction_code] = instruction_object()
+
+class PrefixTypes(IntEnum):
+    FIRST = 0
+    SECOND = 1
+    THIRD = 2
+    FOURTH = 4
+    NONE = 5
+
+class FirstGroupPrefix(IntEnum):
+    LOCK = 0xF0
+    REPNZ = 0xF2
+    REPNE = 0xF2
+    REP = 0xF3
+    BND = 0xF2
+
+    @classmethod
+    def has_value(cls, value: FirstGroupPrefix):
+        return value in set(item.value for item in FirstGroupPrefix)
+
+class SecondGroupPrefix(IntEnum):
+    CS_OVERRIDE = 0x2E
+    SS_OVERRIDE = 0x36
+    DS_OVERRIDE = 0x3E
+    ES_OVERRIDE = 0x26
+    FS_OVERRIDE = 0x64
+    GD_OVERRIDE = 0x65
+    BRANCH_NOT_TAKEN = 0x2E
+    BRANCH_TAKEN = 0x3E
+
+    @classmethod
+    def has_value(cls, value: SecondGroupPrefix):
+        return value in set(item.value for item in SecondGroupPrefix)
+    
+class ThirdGroupPrefix(IntEnum):
+    OPERAND_SIZE_OVERRIDE = 0x66
+
+    @classmethod
+    def has_value(cls, value: ThirdGroupPrefix):
+        return value in set(item.value for item in ThirdGroupPrefix)
+
+class FourthGroupPrefix(IntEnum):
+    ADDRESS_SIZE_OVERRIDE = 0x67
+
+    @classmethod
+    def has_value(cls, value: FourthGroupPrefix):
+        return value in set(item.value for item in FourthGroupPrefix)
 
 class Instruction(metaclass=abc.ABCMeta):
     @classmethod
@@ -48,15 +106,4 @@ class Instruction(metaclass=abc.ABCMeta):
     @abc.abstractmethod
     def execute(self, proc: Processor, args: list):
         raise NotImplementedError
-
-instruction_code_to_instruction_object: Dict[InstructionCodes, Instruction] = {}
-
-def register_instruction(instruction_code: InstructionCodes, instruction_object: Type[Instruction]):
-    global instruction_code_to_instruction_object
-    
-    # First make sure that the opcode exists
-    if not InstructionCodes.has_value(instruction_code):
-        raise InvalidInstructionCode(instruction_code)
-    # If so, save it
-    instruction_code_to_instruction_object[instruction_code] = instruction_object()
 
